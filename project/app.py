948b282101da0ccfc04b6586d82fd720354f8073
welcomevideogame <99519510+welcomevideogame@users.noreply.github.com>
2023-01-13 21:07:07 -0500

    Multiple Page Support
    Application now scrapes multiple pages as long as there are some

--- project/app.py
+++ project/app.py
@@ -4,34 +4,49 @@ from utilities import Utilities
 
 class App:
 
+    url = None
+
     def __init__(self):
-        mc_scraper = Scraper.MicroCenter()
-        options = {"Sort by price": False, "Filter out store only": False, "Simple view": False}
+        
         while True:
-            url = input("Enter a url to scrape: ")
-            if "microcenter" in url:
-                mc_scraper.set_url(url)
-                mc_scraper.scrape_items()
-                while True:
-                    os.system('cls')
-                    for option in options:
-                        print("[" + str(list(options).index(option) + 1) + "] " + option + ": " + str(options[option]))
-                    
-                    choice = input("Enter a number to toggle an option: ")
-                    choice = Utilities.try_parse_int(choice)
-                    if choice:
-                        choice -= 1
-                        if choice in range(len(options)):
-                            options[list(options)[choice]] = not options[list(options)[choice]]
-                    else:
-                        break
-                mc_scraper.print_items(options)
-                save = input("Save to csv? (y/n): ").upper()
-                if save == "Y":
-                    filename = input("Enter a filename: ")
-                    Utilities.save_to_csv(filename, mc_scraper.products)
-                mc_scraper.clear_items()
-            elif url == "0":
+            self.url = input("Enter a url to scrape: ")
+            if "microcenter" in self.url:
+                self.microcenter()
+            elif self.url == "0":
                 break
             else:
                 print("Url not supported.")
+
+    def microcenter(self):
+        options = {"Sort by price": False, "Filter out store only": False, "Filter out of stock": False, "Simple view": False, "Show warnings": True}
+        scraper = Scraper.MicroCenter()
+        scraper.set_url(self.url)
+        pagination = self.check_pagination()
+        scraper.scrape_items(pagination)
+
+        while True:
+            os.system('cls')
+            for option in options:
+                print("[" + str(list(options).index(option) + 1) + "] " + option + ": " + str(options[option]))
+            
+            choice = input("Enter a number to toggle an option: ")
+            choice = Utilities.try_parse_int(choice)
+            if choice:
+                choice -= 1
+                if choice in range(len(options)):
+                    options[list(options)[choice]] = not options[list(options)[choice]]
+            else:
+                break
+        if options["Show warnings"]:
+            if not pagination:
+                print("Warning: No Pagination detected. It is recommended to use a link with pagination.\nPress enter to continue.")
+                input()
+        scraper.print_items(options)
+        save = input("Save to csv? (y/n): ").upper()
+        if save == "Y":
+            filename = input("Enter a filename: ")
+            Utilities.save_to_csv(filename, scraper.products)
+        scraper.clear_items()
+
+    def check_pagination(self):
+        return "page=" in self.url
--- project/scraper.py
+++ project/scraper.py
@@ -3,10 +3,14 @@ import requests
 from items import StoreItems
 from utilities import Utilities
 
+from urllib.parse import urlparse, parse_qs
+import urllib
+
 class Scraper:
 
     class MicroCenter:
         products = []
+        items = []
 
         def __init__(self, url):
             self.url = url
@@ -17,10 +21,35 @@ class Scraper:
         def set_url(self, url):
             self.url = url
     
-        def scrape_items(self):
-            response = requests.get(self.url)
-            soup = BeautifulSoup(response.content, "html.parser")
-            self.items = soup.find_all("li", class_="product_wrapper")
+        def scrape_items(self, pagination):
+            url_list = [self.url]
+            if pagination:
+                url = urlparse(self.url)
+                query_params = parse_qs(url.query)
+                current_page = int(query_params['page'][0])
+                left, right = current_page, 100
+                mid = 0
+                while left <= right:
+                    mid = (left + right) // 2
+                    query_params['page'] = mid
+                    new_url = url._replace(query=urllib.parse.urlencode(query_params, doseq=True))
+                    response = requests.get(new_url.geturl())
+                    soup = BeautifulSoup(response.content, "html.parser")
+                    temp_items = soup.find_all("li", class_="product_wrapper")
+                    if temp_items:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+                for i in range(current_page, mid):
+                    query_params['page'] = i
+                    new_url = url._replace(query=urllib.parse.urlencode(query_params, doseq=True))
+                    url_list.append(new_url.geturl())
+            for url in url_list:
+                response = requests.get(url)
+                soup = BeautifulSoup(response.content, "html.parser")
+                temp_items = soup.find_all("li", class_="product_wrapper")
+                if temp_items:
+                    self.items.extend(temp_items)
             self.make_items_obj()
 
         def make_items_obj(self):
@@ -41,11 +70,12 @@ class Scraper:
                 return storeOnly, inventory
             else:
                 return False, 0
-                
 
         def print_items(self, options):
             if options["Filter out store only"]:
                 self.products = [product for product in self.products if not product.get_storeOnly()]
+            if options["Filter out of stock"]:
+                self.products = [product for product in self.products if product.get_inventory() != 0]
             if options["Sort by price"]:
                 Utilities.sort_by_price(self.products)
             if options["Simple view"]:
